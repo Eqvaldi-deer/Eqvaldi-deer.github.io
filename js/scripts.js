2e6cabe86651e7f339d50c96eb932822b7f2f1e0
BeanGreen247 <mozdrent@gmail.com>
2023-07-13 19:34:17 +0200

    js fix

--- js/scripts.js
+++ js/scripts.js
@@ -40,17 +40,20 @@ function switchTheme() {
     const html = document.querySelector('html');
     const body = document.querySelector('body');
     const capsule = document.querySelector('.capsule');
+    const iframe = document.querySelector('iframe');
   
     if (toggleSwitch.checked) {
       html.setAttribute('data-theme', 'light');
       body.setAttribute('data-theme', 'light');
       capsule.setAttribute('data-theme', 'light');
       localStorage.setItem('theme', 'light');
+      iframe.style.backgroundColor = white;
     } else {
       html.setAttribute('data-theme', 'dark');
       body.setAttribute('data-theme', 'dark');
       capsule.setAttribute('data-theme', 'dark');
       localStorage.setItem('theme', 'dark');
+      iframe.style.backgroundColor = white;
     }
   }
   
@@ -59,6 +62,7 @@ function switchTheme() {
     const html = document.querySelector('html');
     const body = document.querySelector('body');
     const capsule = document.querySelector('.capsule');
+    const iframe = document.querySelector('iframe');
   
     toggleSwitch.addEventListener('change', switchTheme);
   
@@ -68,6 +72,7 @@ function switchTheme() {
       html.setAttribute('data-theme', userTheme);
       body.setAttribute('data-theme', userTheme);
       capsule.setAttribute('data-theme', userTheme);
+      iframe.style.backgroundColor = white;
       if (userTheme === 'light') {
         toggleSwitch.checked = true;
       }
@@ -77,6 +82,7 @@ function switchTheme() {
       html.setAttribute('data-theme', osTheme);
       body.setAttribute('data-theme', osTheme);
       capsule.setAttribute('data-theme', osTheme);
+      iframe.style.backgroundColor = white;
       localStorage.setItem('theme', osTheme);
       toggleSwitch.checked = osTheme === 'light';
     }
